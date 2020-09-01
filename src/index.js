01d33fce6e39fa03d19b69248e7fd13fa59af5ef
leka-ren <polafort@gmail.com>
2020-09-01 12:38:16 +0300

    валидация и слежка за изменениями в попапе

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import "./pages/index.css";
 import PopupShow from "./js/PopupShow";
 import PopupFormContent from "./js/PopupFormContent";
+import FormValidation from "./js/FormValidator";
 
 const auth = document.querySelector(".header__authorization-button");
 const popup = document.querySelector(".popup");
 const popupTitle = document.querySelector(".popup__title");
 const popupBtn = document.querySelector(".popup__sign");
 const form = document.querySelector("#form");
+const validateStringError = {
+  validationAbsenceRU: "Обязательное поле",
+  validationLenghtRU: "Должно быть от 2 до 30 символов",
+  validationMailRU: "Неправильный формат email",
+};
+
 const statePopup = new PopupShow(popup);
 const authForm = new PopupFormContent(form);
 
@@ -31,3 +38,15 @@ function popupReg(event) {
 
 auth.addEventListener("click", showPopup);
 popupBtn.addEventListener("click", popupReg);
+
+let mutationObserver = new MutationObserver(function (mutations) {
+  console.log(mutations);
+  mutations.forEach(function (mutation) {
+    const formBtn = form.querySelector(".popup__button");
+    const validation = new FormValidation(form, formBtn, validateStringError);
+  });
+});
+
+mutationObserver.observe(form, {
+  childList: true,
+});
--- src/js/PopupFormContent.js
+++ src/js/PopupFormContent.js
@@ -11,11 +11,12 @@ export default class PopupFormContent extends Render {
     const formAuth = `
         <form class="popup__form" name="authentication">
             <p id="email" class="popup__name-input">Email</p>
-            <input id="email" type="email" name="email" class="popup__input" placeholder="Введите почту" required>
+            <input id="email" type="email" name="email" class="popup__input" placeholder="Введите почту" pattern="[a-zA-Z]{1}([A-Za-z0-9_-])*@[a-zA-Z0-9.-]+\.[a-zA-Z]+" required>
             <span id="email" class='popup__valid email'></span>
             <p id="password" class="popup__name-input">Password</p>
-            <input id="password" type="password" name="password" class="popup__input" placeholder="Введите пароль" required>
+            <input id="password" type="password" name="password" class="popup__input" placeholder="Введите пароль" minlength=2 maxlength=30 required>
             <span id="password" class='popup__valid password'></span>
+            <span id="submit" class='popup__valid'></span>
             <button type='submit' class="popup__button">Войти</button>
         </form>`;
     this.childEl = formAuth;
